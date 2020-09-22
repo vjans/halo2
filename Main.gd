f9109964158f0f345aea491bf8b03fa280f825b6
zerksan <65516742+zerksan@users.noreply.github.com>
2020-09-22 10:25:24 +0200

    Add files via upload

--- Main.gd
+++ Main.gd
@@ -30,7 +30,7 @@ func new_game():
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
-	$MobPath/MobSpawnLocation.offset = randi()
+	#$MobPath/MobSpawnLocation.offset = randi()
 	# Create a Mob instance and add it to the scene.
 	var mob = Mob.instance()
 	add_child(mob)
